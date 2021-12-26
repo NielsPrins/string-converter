8dd527925d9461a404f918e8a9d179eb6ead19d8
NielsPrins <NielsPrins@users.noreply.github.com>
2021-12-26 20:23:10 +0100

    Minor things

--- src/CaseConverter.svelte
+++ src/CaseConverter.svelte
@@ -49,7 +49,7 @@
 <div class="case">{capitalizeFirstLetter(toCase)} case</div>
 <div class="converted-string">
   {convertedString}
-  <svg on:click={copyText} on:keyup={copyKeyUp} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" tabindex="0">
+  <svg on:click={copyText} on:keyup={copyKeyUp} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" tabindex="0">
     <path
       d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z"/>
   </svg>
@@ -62,7 +62,7 @@
         align-items: center;
         padding: 0.5rem;
         border: 1px solid #ced4da;
-        font-size: 1.2em;
+        font-size: 1.25em;
     }
 
     .case {
